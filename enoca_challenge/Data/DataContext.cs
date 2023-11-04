14479332d766549429d0fefd3ceba585f5636573
Canberk Aykanat <kayra.aykanat@gmail.com>
2023-11-04 18:19:50 +0300

    db script
    db script

--- enoca_challenge/Data/DataContext.cs
+++ enoca_challenge/Data/DataContext.cs
@@ -3,19 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace enoca_challenge.Data
 {
-    public class DataContext:DbContext
+    public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-
         }
-
         public DbSet<Carrier> Carriers { get; set; }
-        public DbSet<CarrierConfiguration> CarriersConfigurations { get; set;}
+        public DbSet<CarrierConfiguration> CarriersConfigurations { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<CarrierReport> CarrierReports { get; set; }
     }
